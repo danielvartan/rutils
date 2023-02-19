5ac9f21669da6256bc182e5185c6e2d70c788db2
Daniel Vartanian <danvartan@gmail.com>
2023-02-19 05:11:48 -0300

    Fix 'na_as()' documentation

--- R/na_as.R
+++ R/na_as.R
@@ -4,11 +4,13 @@
 #'
 #' `r lifecycle::badge("maturing")`
 #'
-#' `na_as()` returns a `NA` value of the same characteristics of an R object.
-#' It really helps when you need to assign `NA`s dynamically.
+#' `na_as()` returns a `NA` value with the same he same class and attributes of
+#' an R object.
+#'
+#' This function was made to facilitate assigning `NA`s dynamically.
 #'
 #' @param x An [atomic vector][checkmate::test_atomic_vector()], provided that
-#'   `na_as()` have a method for it.
+#'   `na_as()` has a method for it.
 #'
 #' @return A `NA` value with the same class and attributes of `x`.
 #'
@@ -29,37 +31,57 @@ na_as <- function(x) {
     UseMethod("na_as")
 }
 
+#' @rdname na_as
 #' @export
 na_as.logical <- function(x) as.logical(NA)
+
+#' @rdname na_as
 #' @export
 na_as.character <- function(x) as.character(NA)
+
+#' @rdname na_as
 #' @export
 na_as.integer <- function(x) as.integer(NA)
+
+#' @rdname na_as
 #' @export
 na_as.numeric <- function(x) as.numeric(NA)
+
+#' @rdname na_as
 #' @export
 na_as.Duration <- function(x) lubridate::as.duration(NA)
+
+#' @rdname na_as
 #' @export
 na_as.Period <- function(x) lubridate::as.period(NA)
 
+#' @rdname na_as
 #' @export
 na_as.difftime <- function(x) {
     as.difftime(as.numeric(NA), units = attributes(x)$units)
 }
 
-#' @export
-na_as.hms <- function(x) hms::as_hms(NA)
-#' @export
-na_as.Date <- function(x) as.Date(NA)
+#' @rdname na_as
 #' @export
 na_as.hms <- function(x) hms::as_hms(NA)
 
+#' @rdname na_as
+#' @export
+na_as.Date <- function(x) as.Date(NA)
+
+#' @rdname na_as
+#' @export
+na_as.hms <- function(x) hms::as_hms(NA)
+
+#' @rdname na_as
 #' @export
 na_as.POSIXct <- function(x) as.POSIXct(NA, tz = attributes(x)$tzone)
 
+#' @rdname na_as
 #' @export
 na_as.POSIXlt <- function(x) as.POSIXlt(NA, tz = attributes(x)$tzone)
 
+#' @rdname na_as
 #' @export
 na_as.Interval <- function(x) {
     lubridate::interval(NA, NA, tz = attributes(x)$tzone)
