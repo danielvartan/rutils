d660db6bf4c1b78c12ef882d868a3cb1009554d8
Daniel Vartanian <danvartan@gmail.com>
2023-09-28 01:44:13 -0300

    Add get_value_between_tags()

--- R/quarto.R
+++ R/quarto.R
@@ -467,6 +467,40 @@ find_between_tags_and_apply <- function(
 # library(checkmate)
 # library(cli)
 
+get_value_between_tags <- function(
+    x,
+    begin_tag = "%:::% .common h1 begin %:::%",
+    end_tag = "%:::% .common h1 end %:::%") {
+  checkmate::assert_character(x)
+  checkmate::assert_string(begin_tag)
+  checkmate::assert_string(end_tag)
+
+  if (length(x) == 1 && checkmate::test_file_exists(x)) {
+    x <- readLines(x)
+  }
+
+  begin_index <- grep(begin_tag, x = x)
+  end_index <- grep(end_tag, x = x)
+
+  if (length(begin_index) == 0 || length(end_index) == 0) {
+    cli::cli_abort("One or both of the tags were not found.")
+  }
+
+  if (!length(begin_index) == 1 || !length(end_index) == 1) {
+    cli::cli_abort(
+      paste0(
+        "More than one tag with the same value was found. ",
+        "Tags must be unique."
+      )
+    )
+  }
+
+  x[inbetween_integers(begin_index, end_index)]
+}
+
+# library(checkmate)
+# library(cli)
+
 transform_value_between_tags <- function(
     x,
     fun,
@@ -476,6 +510,10 @@ transform_value_between_tags <- function(
   checkmate::assert_multi_class(fun, c("character", "function"))
   checkmate::assert_string(begin_tag)
   checkmate::assert_string(end_tag)
+
+  if (length(x) == 1 && checkmate::test_file_exists(x)) {
+    x <- readLines(x)
+  }
 
   begin_index <- grep(begin_tag, x = x)
   end_index <- grep(end_tag, x = x)
