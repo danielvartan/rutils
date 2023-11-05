e2377fa757c1405274e550c89c85011ad046506e
Daniel Vartanian <danvartan@gmail.com>
2023-11-04 21:37:34 -0300

    Fix clean_quarto_mess()

--- R/quarto.R
+++ R/quarto.R
@@ -331,9 +331,7 @@ clean_quarto_mess <- function(file = NULL,
   checkmate::assert_string(wd)
   checkmate::assert_directory_exists(wd)
   checkmate::assert_character(file, null.ok = TRUE)
-  for (i in file) checkmate::assert_file_exists(file.path(wd, i))
   checkmate::assert_character(dir)
-  for (i in dir) checkmate::assert_directory_exists(file.path(wd, i))
   checkmate::assert_character(ext, null.ok = TRUE)
   checkmate::assert_character(ignore, null.ok = TRUE)
 
@@ -344,13 +342,17 @@ clean_quarto_mess <- function(file = NULL,
 
   if (!length(ext_files) == 0) file <- file |> append(ext_files)
 
-  for (i in file[!file %in% ignore]) {
+  if (!is.null(ignore)) {
+    file <- file |> stringr::str_subset(ignore, negate = TRUE)
+  }
+
+  for (i in file) {
     if (checkmate::test_file_exists(file.path(wd, i))) {
       delete_file(i)
     }
   }
 
-  for (i in dir[!dir %in% ignore]) {
+  for (i in dir) {
     if (checkmate::test_directory_exists(file.path(wd, i))) {
       delete_dir(i)
     }
