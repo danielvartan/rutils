1d47621a7717279b60c2dc5a2f664a780f245367
Daniel Vartanian <danvartan@gmail.com>
2024-10-01 06:28:31 -0300

    Move functions `cutter()` and `normalize_extdata()` to `groomr` package

--- R/deprecated.R
+++ R/deprecated.R
@@ -538,3 +538,178 @@ check_temporal <- function(x, any.missing = TRUE, null.ok = FALSE,
 }
 
 assert_temporal <- checkmate::makeAssertionFunction(check_temporal)
+
+# {grommr} -----
+
+normalize_extdata <- function(package = NULL) {
+  if (is.null(package)) package <- get_package_name()
+  assert_namespace(package)
+
+  extdata <- system.file("inst/extdata", package = package)
+  path <- list.dirs(extdata)
+
+  for (i in path) {
+    i <- paste0(i, "/")
+
+    if (length(list.files(i)) == 0) {
+      next
+    } else {
+      new_name <- tolower(list.files(i))
+      new_name <- stringr::str_replace_all(new_name, " - ", "_")
+      new_name <- stringr::str_replace_all(new_name, " ", "-")
+      file.rename(paste0(i, list.files(i)), paste0(i, new_name))
+    }
+  }
+
+  invisible(NULL)
+}
+
+cutter <- function(x, index, between = NULL, rm_start = FALSE,
+                   rm_end = FALSE) {
+  checkmate::assert_atomic_vector(x, min.len = 1)
+  checkmate::assert_integerish(index, lower = 1, upper = length(x),
+                               any.missing = FALSE, all.missing = FALSE,
+                               unique = TRUE)
+  checkmate::assert_choice(between, c("left", "right"), null.ok = TRUE)
+  checkmate::assert_flag(rm_start)
+  checkmate::assert_flag(rm_end)
+
+  if (is.null(between)) {
+    x <- cut_by(x, index)
+  } else {
+    x <- cut_between(x, index, between)
+  }
+
+  if ((isTRUE(rm_start) || isTRUE(rm_end)) && length(x) == 1) {
+    cli::cli_abort(paste0(
+      "The cut process returned just one piece. In those cases, ",
+      "{cli::col_blue('rm_start')} and {cli::col_blue('rm_end')} ",
+      "cannot be {cli::col_red('TRUE')}."
+    ))
+  }
+
+  if (isTRUE(rm_start) && !length(x) == 1) {
+    x <- x[-1]
+  }
+
+  if (isTRUE(rm_end) && !length(x) == 1) {
+    x <- x[-length(x)]
+  }
+
+  x
+}
+
+cut_into_parts <- function(x, n) {
+  checkmate::assert_integerish(n, lower = 2)
+  checkmate::assert_atomic(x, min.len = n)
+
+  cut_size <- ceiling(length(x) / n)
+  cut_indexes <- cut_size
+
+  for (i in utils::head(seq(n)[-1], -1)) {
+    cut_indexes <- append(
+      cut_indexes,
+      cut_indexes[length(cut_indexes)] + cut_size
+    )
+  }
+
+  cutter(x, cut_indexes, between = "right")
+}
+
+cut_between <- function(x, index, between) {
+  checkmate::assert_atomic_vector(x, min.len = 1)
+  checkmate::assert_integerish(index, lower = 1, upper = length(x),
+                               any.missing = FALSE, all.missing = FALSE,
+                               unique = TRUE)
+  checkmate::assert_choice(between, c("left", "right"))
+
+  out <- list()
+
+  if (between == "left") {
+    for (i in index) {
+      i_index <- which(index == i)
+      j <- length(out) + 1
+
+      if (i == index[1]) {
+        if (!i == 1) {
+          out[[1]] <- x[seq(1, i - 1)]
+          j <- 2
+        }
+
+        if (length(index) == 1) {
+          out[[j]] <- x[seq(i, length(x))]
+        } else {
+          out[[j]] <- x[seq(i, index[i_index + 1] - 1)]
+        }
+      } else if (i == index[length(index)]) {
+        out[[j]] <- x[seq(i, length(x))]
+      } else {
+        out[[j]] <- x[seq(i, index[i_index + 1] - 1)]
+      }
+    }
+  } else {
+    for (i in index) {
+      i_index <- which(index == i)
+      j <- length(out) + 1
+
+      if (i == index[1]) {
+        out[[1]] <- x[seq(1, i)]
+
+        if (length(index) == 1) {
+          out[[2]] <- x[seq(i + 1, length(x))]
+        }
+      } else if (i == index[length(index)] && !i == length(x)) {
+        out[[j]] <- x[seq(index[i_index - 1] + 1, i)]
+        out[[j + 1]] <- x[seq(i + 1, length(x))]
+      } else {
+        out[[j]] <- x[seq(index[i_index - 1] + 1, i)]
+      }
+    }
+  }
+
+  out
+}
+
+cut_by <- function(x, index) {
+  checkmate::assert_atomic_vector(x, min.len = 1)
+  checkmate::assert_integerish(index, lower = 1, upper = length(x),
+                               any.missing = FALSE, all.missing = FALSE,
+                               unique = TRUE)
+
+  if (index[1] == 1 || index[length(index)] == length(x)) {
+    cli::cli_abort(paste0(
+      "When {cli::col_red('between = NULL')}, an index cannot be ",
+      "in the start or at the end of an object."
+    ))
+  }
+
+  if (any(Reduce("-", index) == -1)) {
+    cli::cli_abort(paste0(
+      "When {cli::col_red('between = NULL')}, indexes must have at ",
+      "least a distance of {cli::col_red(1)} between each other."
+    ))
+  }
+
+  out <- list()
+
+  for (i in index) {
+    i_index <- which(index == i)
+    j <- length(out) + 1
+
+    if (i == index[1]) {
+      out[[j]] <- x[seq(1, i - 1)]
+
+      if (length(index) == 1) {
+        out[[j + 1]] <- x[seq(i + 1, length(x))]
+      } else {
+        out[[j + 1]] <- x[seq(i + 1, index[i_index + 1] - 1)]
+      }
+    } else if (i == index[length(index)]) {
+      out[[j]] <- x[seq(i + 1, length(x))]
+    } else {
+      out[[j]] <- x[seq(i + 1, index[i_index + 1] - 1)]
+    }
+  }
+
+  out
+}
