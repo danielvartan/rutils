884cb4ff2b718d2c963fe9388fb8be77b3ed867d
Daniel Vartanian <danvartan@gmail.com>
2025-03-09 20:46:10 -0300

    Fix swap_if()

--- R/swap_if.R
+++ R/swap_if.R
@@ -7,7 +7,7 @@
 #' `swap_if()` swaps two values if a condition is met.
 #'
 #' @param x Any type of R object.
-#' @param y Any type of R object.
+#' @param y An R object of the same class and length as `x`.
 #' @param condition A [`logical`][base::logical] flag indicating if the
 #'   values should be swapped (Default: `TRUE`).
 #'
@@ -30,16 +30,45 @@
 #' #>
 #' #> $y
 #' #> [1] 2
+#'
+#' swap_if(letters, LETTERS, c(rep(TRUE, 13), rep(FALSE, 13)))
+#' #> $x
+#' #>  [1] "A" "B" "C" "D" "E" "F" "G" "H" "I" "J" "K" "L" "M" "n"
+#' #> [15] "o" "p" "q" "r" "s" "t" "u" "v" "w" "x" "y" "z"
+#' #>
+#' #> $y
+#' #>  [1] "a" "b" "c" "d" "e" "f" "g" "h" "i" "j" "k" "l" "m" "N"
+#' #> [15] "O" "P" "Q" "R" "S" "T" "U" "V" "W" "X" "Y" "Z"
 swap_if <- function(x, y, condition = TRUE) {
-  checkmate::assert_flag(condition)
+  # prettycheck::assert_identical(x, y, type = "class")
+  # prettycheck::assert_identical(x, y, condition, type = "length")
+  checkmate::assert_logical(condition)
+
+  if (class(x) != class(y)) {
+    cli::cli_abort(
+      paste0(
+        "{.strong {cli::col_red('x')}} and ",
+        "{.strong {cli::col_red('y')}} must be of the same class."
+      )
+    )
+  }
+
+  if (length(x) != length(y) || length(x) != length(condition)) {
+    cli::cli_abort(
+      paste0(
+        "{.strong {cli::col_red('x')}}, ",
+        "{.strong {cli::col_red('y')}} and, ",
+        "{.strong {cli::col_red('condition')}} ",
+        "must have the same length."
+      )
+    )
+  }
 
   first_arg <- x
   second_arg <- y
 
-  if (isTRUE(condition)) {
-    x <- second_arg
-    y <- first_arg
-  }
+  x <- dplyr::if_else(condition, second_arg, first_arg)
+  y <- dplyr::if_else(condition, first_arg, second_arg)
 
   list(x = x, y = y)
 }
--- tests/testthat/test-swap_if.R
+++ tests/testthat/test-swap_if.R
@@ -7,9 +7,20 @@ test_that("swap_if() | General test", {
 
   swap_if(x = 5, y = 1, condition = 2 > 1) |>
     expect_equal(list(x = 1, y = 5))
+
+  swap_if(letters, LETTERS, rep(TRUE, length(letters))) |>
+    expect_equal(list(x = LETTERS, y = letters))
 })
 
 test_that("swap_if() | Error test", {
+  # if (class(x) != class(y)) ...
+  swap_if(x = 1, y = "1", condition = TRUE) |>
+    expect_error()
+
+  # if (length(x) != length(y) || length(x) != length(condition)) ...
+  swap_if(x = 1, y = 1:2, condition = TRUE) |>
+    expect_error()
+
   # checkmate::assert_logical(condition)
   swap_if(x = 1, y = 1, condition = 1) |>
     expect_error("Assertion on 'condition' failed")
