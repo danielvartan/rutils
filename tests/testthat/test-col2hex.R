fa1a9069355f60cf7026e438b55cd8e2b1d04c00
Daniel Vartanian <danvartan@gmail.com>
2025-03-08 23:33:08 -0300

    Update col2hex()

--- tests/testthat/test-col2hex.R
+++ tests/testthat/test-col2hex.R
@@ -3,9 +3,18 @@ testthat::test_that("col2hex() | General test", {
 
   col2hex(c("red", "green", "blue")) |>
     testthat::expect_equal(c("#FF0000", "#00FF00", "#0000FF"))
+
+  col2hex(c("red", "#000000")) |>
+    testthat::expect_equal(c("#FF0000", "#000000"))
+
+  col2hex(c("red", "#000000FF")) |>
+    testthat::expect_equal(c("#FF0000", "#000000FF"))
 })
 
 testthat::test_that("col2hex() | Error test", {
   # checkmate::assert_character(x)
   col2hex(1) |> testthat::expect_error()
+
+  # assert_color(x)
+  col2hex("a") |> testthat::expect_error()
 })
