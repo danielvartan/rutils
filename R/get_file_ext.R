f1726c3bb281afa8d9b270495584220519d897d4
Daniel Vartanian <danvartan@gmail.com>
2025-03-16 05:34:13 -0300

    Export get_file_ext()

--- R/get_file_ext.R
+++ R/get_file_ext.R
@@ -1,10 +1,30 @@
-# library(checkmate)
+#' Get file extension
+#'
+#' @description
+#' `get_file_ext()` is similar to [`file_ext()`][tools::file_ext], but it
+#' returns the file extension with the dot separator (e.g., `.csv`).
+#'
+#' @param file A [`character`][base::character] string with the file paths.
+#'
+#' @return A [`character`][base::character] string containing the file
+#'   extension of the files.
+#'
+#' @family string functions
+#' @export
+#'
+#' @examples
+#' get_file_ext("example.txt")
+#' #> [1] ".txt" # Expected
+#'
+#' get_file_ext("/path/to/file.tar.gz")
+#' #> [1] ".gz"  # Expected
+#'
+#' get_file_ext(c("data.csv", "data.rds"))
+#' #> [1] ".csv" ".rds" # Expected
+get_file_ext <- function(file) {
+  checkmate::assert_character(file)
 
-# Similar to `tools::file_ext()` but returns the file extension with the dot.
-get_file_ext <- function(file_path) {
-  checkmate::assert_string(file_path)
-
-  file_path |>
+  file |>
     basename() |>
     stringr::str_extract("\\.[[:alnum:]]+$")
 }
