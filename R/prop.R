a4a26f7098eca7f17133f1da6e3f2efd999361be
Daniel Vartanian <danvartan@gmail.com>
2025-06-05 06:32:54 -0300

    Update `prop()`

--- R/prop.R
+++ R/prop.R
@@ -5,8 +5,9 @@
 #' `prop()` computes the proportion of specified values in a vector.
 #'
 #' @param x An [`atomic`][base::atomic] vector.
-#' @param value An [`atomic`][base::atomic] vector containing the unique values
-#'   in `x` for which to compute proportions.
+#' @param value An [`atomic`][base::atomic] vector containing values for which
+#'   to compute proportions. If `value` is not present in `x`, the
+#'   function will return `NA` for that value.
 #' @param na_rm A [`logical`][base::logical] flag indicating whether to
 #'   remove `NA` values from `x` before computing proportions (default: `TRUE`).
 #'
@@ -20,19 +21,26 @@
 #' prop(c("a", "b", "a", "c", "b", "a"), c("a", "b"))
 #' #> [1] 0.500000 0.3333333
 #'
-#' prop(c(1, 2, 1, 2, 1), c(1, 2))
-#' #> [1] 0.6 0.4
+#' prop(c(1, 2, 1, 2, 1), c(1, 2, 5))
+#' #> [1] 0.6 0.4 NA
 #'
 #' prop(c(TRUE, TRUE, TRUE, FALSE), c(TRUE, FALSE))
 #' #> [1] 0.75 0.25
 prop <- function(x, value, na_rm = TRUE) {
   checkmate::assert_atomic_vector(x)
-  checkmate::assert_atomic(value, unique = TRUE)
   prettycheck::assert_identical(x, value, type = "class")
-  checkmate::assert_subset(value, unique(x))
   checkmate::assert_flag(na_rm)
 
   if (na_rm) x <- x[!is.na(x)]
 
-  purrr::map_dbl(value, \(value) length(which(x == value)) / length(x))
+  value |>
+    purrr::map_dbl(
+      function(value) {
+        if (!value %in% x) {
+          0
+        } else {
+          length(which(x == value)) / length(x)
+        }
+      }
+    )
 }
